6396e4a51a1848a87a3f8dacff69b548ea7735ea
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2020-09-30 12:21:36 -0400

    Update entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,6 +17,21 @@ fi
 
 # If we do not need to register a service just run the command
 if [ ! -z "$SERVICE_CONFIG" ]; then
+  if [ -z "$CONSUL_SERVER_ADDR"]; then
+    echo "Specify CONSUL_SERVER_ADDR for certificate..."
+    exit 1
+  fi
+
+  if [ -z "$CONSUL_CACERT"]; then
+    echo "Specify CONSUL_CACERT for certificate..."
+    exit 1
+  fi
+
+  echo "Retrieve server certificate"
+  curl -s ${CONSUL_SERVER_ADDR}:8500/v1/connect/ca/roots | \
+  jq -r '.ActiveRootID as $activeRoot | .Roots | map(select(.ID == $activeRoot)) | .[0].RootCert' | \
+  sed '/^[[:space:]]*$/d' > ${CONSUL_CACERT}
+
   export CONSUL_HTTP_ADDR=https://$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4):8501
   echo ${CONSUL_SERVICE_CONFIG} | base64 -d > ${SERVICE_CONFIG}
   # Wait until Consul can be contacted
@@ -39,41 +54,6 @@ if [ ! -z "$SERVICE_CONFIG" ]; then
   
   # make sure the service deregisters when exit
   trap "consul services deregister ${SERVICE_CONFIG}" SIGINT SIGTERM EXIT
-fi
-
-# register any central config from individual files
-if [ ! -z "$CENTRAL_CONFIG" ]; then
-  IFS=';' read -r -a configs <<< ${CENTRAL_CONFIG}
-
-  for file in "${configs[@]}"; do
-    echo "Writing central config $file"
-    consul config write $file
-     
-    exit_status=$?
-    if [ $exit_status -ne 0 ]; then
-      echo "### Error writing central config: $file ###"
-      cat $file
-      echo ""
-      exit 1
-    fi
-  done
-fi
-
-# register any central config from a folder
-if [ ! -z "$CENTRAL_CONFIG_DIR" ]; then
-  for file in `ls -v $CENTRAL_CONFIG_DIR/*`; do 
-    echo "Writing central config $file"
-    consul config write $file
-    echo ""
-
-    exit_status=$?
-    if [ $exit_status -ne 0 ]; then
-      echo "### Error writing central config: $file ###"
-      cat $file
-      echo ""
-      exit 1
-    fi
-  done
 fi
 
 # Run the command if specified
