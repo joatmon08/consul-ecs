2742862c03a80f381bd6241631372f7d9ca687da
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2020-10-01 11:00:12 -0400

    Update to not use leaf certificates

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+export CONSUL_HTTP_SSL=true
+
 if [ ! -z "$CONSUL_CERTIFICATE" ]; then
   if [ ! -z "$CONSUL_CA_FILE_PATH" ]; then
     echo "Saving consul certificate..."
@@ -7,9 +9,25 @@ if [ ! -z "$CONSUL_CERTIFICATE" ]; then
   fi
 fi
 
+if [ -z "$CONSUL_HTTP_TOKEN" ]; then
+  echo "Set CONSUL_HTTP_TOKEN..."
+fi
+
+if [ -z "$CONSUL_HTTP_ADDR" ]; then
+  export CONSUL_HTTP_ADDR=https://$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4):8501
+fi
+
+if [ -z "$CONSUL_CACERT" ]; then
+  echo "Set CONSUL_CACERT..."
+fi
+
+echo "Retrieve server certificate..."
+curl -s -k -H "X-Consul-Token:${CONSUL_HTTP_TOKEN}" ${CONSUL_HTTP_ADDR}/v1/connect/ca/roots | \
+jq -r '.ActiveRootID as $activeRoot | .Roots | map(select(.ID == $activeRoot)) | .[0].RootCert' | \
+sed '/^[[:space:]]*$/d' > ${CONSUL_CACERT}
+
 if [ ! -z "$CONSUL_CLIENT_CONFIG" ]; then
   EC2_HOST_ADDRESS=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)
-  EC2_NODE_NAME=$(curl -s http://169.254.169.254/latest/meta-data/local-hostname  | cut -f1 -d.)
   echo "Using EC2 host address ${EC2_HOST_ADDRESS}..."
   echo "Using EC2 node name ${EC2_NODE_NAME}..."
   echo $CONSUL_CLIENT_CONFIG | base64 -d | jq '.auto_encrypt.ip_san = ['\"$EC2_HOST_ADDRESS\"'] | .advertise_addr = '\"$EC2_HOST_ADDRESS\"'' > /consul/config/client.json
@@ -17,27 +35,17 @@ fi
 
 # If we do not need to register a service just run the command
 if [ ! -z "$SERVICE_CONFIG" ]; then
-  if [ -z "$CONSUL_CACERT" ]; then
-    echo "Specify CONSUL_CACERT for certificate file..."
-    exit 1
-  fi
-
-  export CONSUL_HTTP_SSL=true
-  export CONSUL_HTTP_ADDR=https://$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4):8501
   export CONSUL_GRPC_ADDR=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4):8502
 
-  echo ${CONSUL_SERVICE_CONFIG} | base64 -d > ${SERVICE_CONFIG}
+  IP_ADDR=$(ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}')
+
+  echo ${CONSUL_SERVICE_CONFIG} | base64 -d | sed "s/##CONTAINER_IP##/${IP_ADDR}/" > ${SERVICE_CONFIG}
   # Wait until Consul can be contacted
   until curl -s -k ${CONSUL_HTTP_ADDR}/v1/status/leader | grep 8300; do
     echo "Waiting for Consul to start at ${CONSUL_HTTP_ADDR}..."
     sleep 1
   done
 
-  echo "Retrieve server certificate..."
-  curl -s -k ${CONSUL_HTTP_ADDR}/v1/connect/ca/roots | \
-  jq -r '.ActiveRootID as $activeRoot | .Roots | map(select(.ID == $activeRoot)) | .[0].RootCert' | \
-  sed '/^[[:space:]]*$/d' > ${CONSUL_CACERT}
-    # register the service with consul
   echo "Registering service with consul ${SERVICE_CONFIG}..."
   consul services register ${SERVICE_CONFIG}
   
@@ -48,19 +56,6 @@ if [ ! -z "$SERVICE_CONFIG" ]; then
     echo ""
     exit 1
   fi
-
-  export CONSUL_CLIENT_CERT="/tmp/consul-ca-service.pem"
-  export CONSUL_CLIENT_KEY="/tmp/consul-ca-service-key.pem"
-  export CONSUL_SERVICE_NAME=$(jq -r '.service.name' ${SERVICE_CONFIG})
-
-  echo "Retrieve leaf certificate for ${CONSUL_SERVICE_NAME}..."
-  curl -s -k -H "X-Consul-Token:${CONSUL_HTTP_TOKEN}" ${CONSUL_HTTP_ADDR}/v1/agent/connect/ca/leaf/${CONSUL_SERVICE_NAME} | \
-  jq -r '.CertPEM' | \
-  sed '/^[[:space:]]*$/d' > ${CONSUL_CLIENT_CERT}
-  
-  curl -s -k -H "X-Consul-Token:${CONSUL_HTTP_TOKEN}" ${CONSUL_HTTP_ADDR}/v1/agent/connect/ca/leaf/${CONSUL_SERVICE_NAME} | \
-  jq -r '.PrivateKeyPEM' | \
-  sed '/^[[:space:]]*$/d' > ${CONSUL_CLIENT_KEY}
   
   # make sure the service deregisters when exit
   trap "consul services deregister ${SERVICE_CONFIG}" SIGINT SIGTERM EXIT
